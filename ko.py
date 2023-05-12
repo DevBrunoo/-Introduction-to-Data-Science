a620fb81e182a5ceb83095d10ea84731f8d929ef
Bruno Oliveira <brunocosta1699320@gmail.com>
2023-05-11 23:13:04 -0300

    add other

---
+++ ko.py
@@ -0,0 +1,9 @@
+import csv
+
+files = ["./dados1/paises.csv", "./dados1/sociais.csv"]
+
+for filename in files:
+  with open(filename, "r") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        print(row)
